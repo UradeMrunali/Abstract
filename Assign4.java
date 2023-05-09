469fec67c780779237c5762ebdea6cd54d049d7c
Mrunali <mrunaliurade26@gmail.com>
2023-05-09 17:31:33 +0530

    creating abstract cls

---
+++ Assign4.java
@@ -0,0 +1,36 @@
+
+abstract class Test
+{
+  	Test()
+	{
+		System.out.println("This is constructor of abstract class");
+	}
+  	 abstract void a_method();
+  	 
+  	 void b_method()
+  	 {
+  		 System.out.println("This is normal method of abstract class");
+  	 }
+}
+ class SubClass extends Test
+ {
+
+	@Override
+	void a_method() {
+		System.out.println("This is abstract method");
+		
+	}
+	 
+ }
+
+
+public class Assign4 {
+	public static void main(String[] args) {
+		SubClass s=new SubClass();
+		s.a_method();
+		s.b_method();
+		
+		
+	}
+
+}
